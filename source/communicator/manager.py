67498df0c89f46dbdab9c36874814e82c1e17e17
JYIYIC-P <1467612622@qq.com>
2025-10-31 10:13:57 +0800

    描述更新

--- source/communicator/manager.py
+++ source/communicator/manager.py
@@ -8,22 +8,38 @@ class manager():
     def __init__(self,mode = None):
         """
         传入模式，根据模式判断来创建和管理对象
+        初始化若传入mode 则启动，否则创建none对象
         """
         if mode is not None:
             self.mode = mode
-            if mode == ""
-
+            self.start()
+        else:
+            self.camera0 = None
+            self.camera1 = None
+            self.pcie = None
+            self.hhit = None
         
 
     def start(self):
         """
         判断哪个mode，哪个mode需要哪些通信方式
         """
+        if self.mode is not None:
+            pass
     def stop(self):
         """
         结束线程，但是保留对象
         """
+        if self.mode is not None:
+            pass
+        else :
+            """
+            此处加入对象检测，保证完全关闭
+            """
     def changemode(self,mode):
         """
         根据传入的mode来选择启动和停止哪些对象及其线程
         """
+        self.stop()
+        self.mode = mode
+        self.start()
--- source/main.py
+++ source/main.py
@@ -3,8 +3,27 @@
 从此处打开后端（通信类和逻辑处理类）与前端（PYQT界面）
 
 """
+import pathlib,sys
+root = pathlib.Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(root))
+
 
 import Ui #PYQT界面
 import logic.logic_handler as UPDATE
 #import communicator.
 
+import communicator.manager
+
+def main():
+    """
+    此函数为整个程序的启动入口
+    启动流程：
+    1.应当创建配置管理类，加载配置
+    2.根据配置启动前端：（包含交互界面等信息，只记录信息和改变）
+    3.待前端准备好后，启动通信管理类，只创建对象，不启动线程
+    4.启动逻辑处理类，只创建对象，不启动线程
+    5.主循环（获取前端用户操作，如果操作对应更新）
+    """
+
+if __name__ == "__main__":
+    main()
