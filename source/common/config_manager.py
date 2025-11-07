b82408848fb16af1c43ba2c9c9180c8d7e7f35bf
JYIYIC-P <1467612622@qq.com>
2025-11-07 13:42:42 +0800

    更新设置函数

--- source/common/config_manager.py
+++ source/common/config_manager.py
@@ -26,23 +26,22 @@ class ConfigManager:
             # TypeError: value 不是字典，不能继续用 key 索引（比如 value 是字符串，却还想用 ['a'] 访问）
             return default
 
-    def set(self, key1, key2,key3, value):
+    def set(self, *keys, value):
         with self._lock:
-            # 如果用户配置文件不存在，先将默认配置完全复制过来
             if not self._user_file.exists():
-                self._user_file.parent.mkdir(parents=True, exist_ok=True)  # 确保目录存在
+                self._user_file.parent.mkdir(parents=True, exist_ok=True)
                 self._user_file.write_text(json.dumps(self._cfg, indent=2), encoding='utf-8')
-            # 加载用户配置文件
-            self._cfg = json.loads(self._user_file.read_text(encoding='utf-8'))
-            
-            # 修改配置
-            # if communicator not in self._cfg:
-            #     self._cfg[communicator] = {}
-            self._cfg[key1][key2][key3] = value
-            # 保存修改后的配置
-            self._user_file.write_text(json.dumps(self._cfg, indent=2), encoding='utf-8')
 
+            cfg = self._cfg
+            for key in keys[:-1]:
+                if key not in cfg:
+                    cfg[key] = {}
+                cfg = cfg[key]
+            cfg[keys[-1]] = value
+
+            self._user_file.write_text(json.dumps(self._cfg, indent=2), encoding='utf-8')
 if __name__ == '__main__':
     cfg = ConfigManager()
-    cfg.set("camera", "config","fps", 50)
-    print(cfg.get("hhit_mode"))
+    value={"pt":"aaa"}
+    cfg.set("yolo_mode", "path", value=value)
+    print(cfg.get("yolo_mode"))
