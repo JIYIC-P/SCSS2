b2d3f23f0f74e61fa7c7d7faa7354b196e4a7bed
JYIYIC-P <1467612622@qq.com>
2025-11-09 17:58:55 +0800

    NUM

--- source/logic/logic_handler.py
+++ source/logic/logic_handler.py
@@ -221,8 +221,8 @@ class Updater():
                 self.count+=1
                 self.bus.algo_result.emit({"ID": ID, "count": self.count})
                 '''这里应该返回，还没有结束'''
-                self.bus.camera0_img.emit(QPixmap.fromImage(ndarray_to_qimage(frame))) #发射相机一裁剪后的图片
-                self.bus.camera1_img.emit(QPixmap.fromImage(ndarray_to_qimage(self.frame1))) #发射相机二元数据
+                self.bus.camera0_img.emit(frame) #发射相机一裁剪后的图片
+                self.bus.camera1_img.emit(self.frame1)#发射相机二元数据
                 
                 return {"ID": ID, "count": self.count}
                 #return {"ID": random.randint(1,5), "count": self.count}
@@ -233,13 +233,8 @@ class Updater():
                 _,_,ID=color_mode().match_color(frame_cut0)#返回的有三个值，目前只用ID
                 self.count+=1
                 '''这里应该返回，还没有结束'''
-
-                s = ndarray_to_qimage(self.frame0)
-                print(s.__class__)
-                img = QPixmap.fromImage(s)
-                print(img.__class__)
-                self.bus.camera0_img.emit(img)#发射相机一裁剪后的图片
-                self.bus.camera1_img.emit(QPixmap.fromImage((ndarray_to_qimage(frame_cut0)))) #发射相机二元数据
+                self.bus.camera0_img.emit(self.frame0)#发射相机一裁剪后的图片
+                self.bus.camera1_img.emit(frame_cut0) #发射相机二元数据
 
                 self.bus.algo_result.emit({"ID": ID, "count": self.count})
 
@@ -254,8 +249,8 @@ class Updater():
             vis, _,ID =  clip_mode().match_clip(frame_cut0,frame_cut1)#返回的有四个值，目前只用ID
             self.count+=1
             '''这里应该返回，还没有结束'''
-            self.bus.camera0_img.emit(QPixmap.fromImage(ndarray_to_qimage(vis)) )#发射相机一裁剪后的图片
-            self.bus.camera1_img.emit(QPixmap.fromImage(ndarray_to_qimage(self.frame1))) #发射相机二元数据
+            self.bus.camera0_img.emit(vis)#发射相机一裁剪后的图片
+            self.bus.camera1_img.emit(self.frame1) #发射相机二元数据
             self.bus.algo_result.emit({"ID": ID, "count": self.count})
 
             return {"ID": ID, "count": self.count}
