31e1787ea098510f4bcf295dbae42d2fc82e3b55
JYIYIC-P <1467612622@qq.com>
2025-11-12 09:59:54 +0800

    gai

--- source/logic/logic_handler.py
+++ source/logic/logic_handler.py
@@ -172,7 +172,7 @@ class Updater():
         """
 
         cloth_id = result["ID"] 
-        delay_key=self.bus.cfg.find_key_path(self.bus.cfg(f"{self.mode}_mode"),cloth_id) 
+        delay_key=self.bus.cfg.find_key_path(self.bus.cfg.get(f"{self.mode}_mode"),cloth_id) 
         delay=self.bus.cfg.get(f"{self.mode}_mode","delay",delay_key)        # 衣服类别
         #delays
         for idx, worker_id in enumerate(self.worker):   # worker = [1,2,3,4,5]
